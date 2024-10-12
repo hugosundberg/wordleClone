626d42195e96e39d9fbe9d9dd63f426f033ae9cc
hugosundberg <hugosundberg98@gmail.com>
2024-10-12 20:26:37 +0200

    example words imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-function App() {
+import { useEffect, useState } from "react";
+import "./App.css";
+import { words } from "./words.tsx";
+
+const BASE_URL = "https://api.frontendexpert.io/api/fe/wordle-words";
+
+export default function App() {
+  const exampleWords = [words];
+  const [solution, setSolution] = useState("");
+
   return (
     <>
       <div className="game-body">
         <h1>Wordle Clone</h1>
+        {solution}
         <div className="row">
           <div className="tile"></div>
           <div className="tile"></div>
@@ -42,5 +52,3 @@ function App() {
     </>
   );
 }
-
-export default App;
