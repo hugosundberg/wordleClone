c3350dfb2079e40399dd157de456580e7060268c
hugosundberg <hugosundberg98@gmail.com>
2024-10-12 21:56:32 +0200

    typing implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,61 +2,66 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { words } from "./words.tsx";
 
-const BASE_URL = "https://api.frontendexpert.io/api/fe/wordle-words";
-
 export default function App() {
-  const handleTyping = (event: any) => {
-    console.log(event.key);
-  };
-
-  useEffect(() => {
-    window.addEventListener("keydown", handleTyping);
-  });
-
   const exampleWords = [words];
   const [solution, setSolution] = useState("");
+  const [guesses, setGuesses] = useState(Array(6).fill(null));
+  const [currentGuess, setCurrentGuess] = useState("");
+
+  useEffect(() => {
+    const handleTyping = (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        return;
+      }
+
+      if (event.key === "Backspace") {
+        setCurrentGuess(currentGuess.slice(0, -1));
+        return;
+      }
+
+      setCurrentGuess(currentGuess + event.key);
+    };
+
+    window.addEventListener("keydown", handleTyping);
+
+    return () => {
+      window.removeEventListener("keydown", handleTyping);
+    };
+  }, [currentGuess]);
+  let keyIndex = 0;
 
   return (
     <>
       <div className="game-body">
         <h1>Wordle Clone</h1>
-        {solution}
-        <div className="row">
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-        </div>
-        <div className="row">
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-        </div>
-        <div className="row">
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-        </div>
-        <div className="row">
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-        </div>
-        <div className="row">
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-          <div className="tile"></div>
-        </div>
+        {guesses.map((guess, i) => {
+          const isCurrentGuess = i === guesses.findIndex((val) => val == null);
+          return (
+            <Row
+              guess={isCurrentGuess ? currentGuess : guess ?? ""}
+              key={keyIndex++}
+            />
+          );
+          {
+            currentGuess;
+          }
+        })}
       </div>
     </>
   );
+}
+
+function Row({ guess }: any) {
+  const tiles = [];
+
+  for (let i = 0; i < 5; i++) {
+    const character = guess[i];
+    tiles.push(
+      <div key={i} className="tile">
+        {character}
+      </div>
+    );
+  }
+
+  return <div className="row">{tiles}</div>;
 }
